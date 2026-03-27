c2ab8734507fe573e9c6582abd7a326d3cb3af7a
ramzorevelo <vncramzo@gmail.com>
2026-03-27 09:03:58 +0800

    Change battle to Player vs VampireBat

--- TurnBasedRPG/main.cpp
+++ TurnBasedRPG/main.cpp
@@ -3,6 +3,7 @@
 #include "StoneGolem.h"
 #include "Slime.h"
 #include "Mage.h"
+#include "VampireBat.h"
 #include <iostream>
 #include <string>
 
@@ -18,9 +19,11 @@ int main()
     //StoneGolem stonegolem{ "Stone Golem", 200, 100 };
     //Battle     battle{ player, stonegolem };
     //Mage mage{ "Mage" };
-    Slime slime{ "Slime", 150, 50 };
+    //Slime slime{ "Slime", 150, 50 };
     //Battle     battle{ mage, slime };
-    Battle     battle{ player, slime };
+    //Battle     battle{ player, slime };
+    VampireBat bat{ "Vampire Bat", 100, 40 };
+    Battle battle{ player, bat };
     battle.run();
 
     return 0;
